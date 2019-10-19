61bff539af95c5de6c2b069628f8b4f7d2aeb5a9
Thinh Nguyen <tnntech@gatech.edu>
2019-10-19 04:09:03 -0400

    Added basic sound for rabbit when start moving

---
+++ Assets/Scripts/AppEvents/RabbitSteps.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RabbitSteps : MonoBehaviour
+{
+	CharacterInputController cc;
+	AudioSource audio;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+		cc = GetComponent<CharacterInputController>();
+        audio = GetComponent<AudioSource>();
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+		if (cc.Velocity > 0f && !audio.isPlaying)
+		{
+			audio.Play();
+		}
+	}
+}
