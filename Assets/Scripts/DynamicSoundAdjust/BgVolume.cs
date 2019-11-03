302551b97493ae0815d598bc7c822b1629795a81
Thinh Nguyen <tnntech@gatech.edu>
2019-11-03 00:16:40 -0400

    Updated Game menu, improve UI. Added options(sliders ->> volume + camera turn rate). Added backdrop for game menu

---
+++ Assets/Scripts/DynamicSoundAdjust/BgVolume.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class BgVolume : MonoBehaviour
+{
+    TextMeshProUGUI currentBgVolume;
+    public int backgroundVolume;
+
+    void Start() {
+        currentBgVolume = GetComponent<TextMeshProUGUI> ();
+        backgroundVolume = 100; // Set default value when start game
+        currentBgVolume.text = backgroundVolume.ToString() + "/100";
+    }
+
+    public void textUpdate(float value)
+    {
+        backgroundVolume = Mathf.RoundToInt(value * 100);
+        currentBgVolume.text = backgroundVolume.ToString() + "/100";
+    }
+}
