4b58b140d3932473c4ad2777e4e190b13c2d1f43
Exifience <henrypan1999@gmail.com>
2019-11-11 18:06:13 -0500

    teleport pad

---
+++ Assets/TeleportPad.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportPad : MonoBehaviour
+{
+    public GameObject ui;
+    public GameObject objToTP;
+    public GameObject tpLoc;
+
+    void Start()
+    {
+    	ui.SetActive(false);
+	}
+
+	void OnTriggerStay(Collider other)
+	{
+		ui.SetActive(true);
+		if((other.gameObject.tag == "Detectable") && Input.GetKeyDown(KeyCode.E))
+		{
+			objToTP.transform.position = tpLoc.transform.position;
+		}
+	}
+
+	void OnTriggerExit()
+	{
+		ui.SetActive(false);
+	}
+	
+}
