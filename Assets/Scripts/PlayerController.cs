6732ec0e04c5ba0fd89b437e308d7c75ae8e3a61
Henry Pan <32212826+henrypan1999@users.noreply.github.com>
2019-11-20 18:26:31 -0500

    stamina bar, project setting is 2.5 (#20)

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,16 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
-  public float walkSpeed = 2;
-	public float runSpeed = 6;
+  	private float walkSpeed = 0.55f;
+	private float runSpeed = 1;
 
-	public float turnSmoothTime = 0.2f;
+	private float turnSmoothTime = 0.2f;
 	float turnSmoothVelocity;
 
-	public float speedSmoothTime = 0.1f;
+	private float speedSmoothTime = 0.1f;
 	float speedSmoothVelocity;
 	float currentSpeed;
 
@@ -21,12 +22,21 @@ public class PlayerController : MonoBehaviour
 	private float sleepWaitTime = 2.0f;
 	private float idleSwap = 0.0f;
 	private float timer = 0.0f;
+
+	private float stamina = 2.0f;
+	private float maxStamina = 2.0f;
+
+	public Slider staminaBar;
 	
 	void Start () {
 		animator = GetComponent<Animator> ();
 		cameraT = Camera.main.transform;
+		maxStamina = 2.0f;
+		stamina = 2.0f;
+		staminaBar.value = stamina;
 	}
 
+	
 	void Update () {
 
 		Vector2 input = new Vector2 (Input.GetAxisRaw ("Horizontal"), Input.GetAxisRaw ("Vertical"));
@@ -38,36 +48,64 @@ public class PlayerController : MonoBehaviour
 		}
 
 		bool running = Input.GetKey (KeyCode.LeftShift);
-		float targetSpeed = ((running) ? runSpeed : walkSpeed) * inputDir.magnitude;
-		currentSpeed = Mathf.SmoothDamp (currentSpeed, targetSpeed, ref speedSmoothVelocity, speedSmoothTime);
-
+		if ((stamina > 0) && (running == true)) {
+			float targetSpeed = ((running) ? runSpeed : walkSpeed) * inputDir.magnitude;
+			currentSpeed = Mathf.SmoothDamp (currentSpeed, targetSpeed, ref speedSmoothVelocity, speedSmoothTime);
+			animator.SetBool("IsRun", true);
+			stamina -= Time.deltaTime;
+			staminaBar.value = stamina;
+		} else {
+			running = false;
+			float targetSpeed = walkSpeed * inputDir.magnitude;
+			currentSpeed = Mathf.SmoothDamp (currentSpeed, targetSpeed, ref speedSmoothVelocity, speedSmoothTime);
+			animator.SetBool("IsRun", false);
+		}
+		
+		//stamina
+		if(!running)
+		{
+			if(stamina < 0)
+			{
+				stamina = 0;
+				staminaBar.value = stamina;
+				running = false;
+				animator.SetBool("IsRun", false);
+			}else if (stamina < maxStamina)
+			{
+				stamina += Time.deltaTime/6;
+				staminaBar.value = stamina;
+			}
+		}
+		
+		
 		transform.Translate (transform.forward * currentSpeed * Time.deltaTime, Space.World);
 
 		float animationSpeedPercent = ((running) ? 1 : .5f) * inputDir.magnitude;
 		animator.SetFloat ("speedPercent", animationSpeedPercent, speedSmoothTime, Time.deltaTime);
 
+		
 		if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D))
-      {
+        {
         	animator.SetBool("IsWalk", true);
         	animator.SetBool("IsIdle", false);
         	animator.SetBool("IsSleep", false);
         	isWalking = true;
         	timer = 0.0f;
-      }
-      if (!isWalking)
-      {
+        }
+        if (!isWalking)
+        {
 	    	animator.SetBool("IsWalk", false);
 	    	animator.SetBool("IsIdle", true);
-        timer += Time.deltaTime;
+        	timer += Time.deltaTime;
         	
-        if (timer > sleepWaitTime) {
-				  animator.SetBool("IsSleep", true);
-				  animator.SetBool("IsIdle", false);
+        	if (timer > sleepWaitTime) {
+				animator.SetBool("IsSleep", true);
+				animator.SetBool("IsIdle", false);
+        	}
         }
-      }
-      else
-      {
-        isWalking = false;
-      }
-	}
+        else
+        {
+        	isWalking = false;
+        }
+	  }
 }
